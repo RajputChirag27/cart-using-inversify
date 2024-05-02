f27a05416e2c7fc49a382d2d5e031138b8ea0a1f
RAJPUT CHIRAGSINGH ARJUNSINGH <csronly4@gmail.com>
2024-05-02 21:20:31 +0530

    as

--- src/controllers/ProtectedController.ts
+++ src/controllers/ProtectedController.ts
@@ -2,11 +2,16 @@
 import { Request, Response } from 'express';
 import { controller, httpGet } from 'inversify-express-utils';
 import { authenticateJwt } from '../middleware/authenticateJwt';
+import { AuthenticatedRequest } from 'src/interfaces/AuthenticationInterface';
 
 @controller('/protected')
 export class ProtectedController {
     @httpGet('/', authenticateJwt)
-    protectedRoute(req: Request, res: Response) {
-        res.send('Protected Route');
+    protectedRoute(req: AuthenticatedRequest, res: Response) {
+        try{
+            res.send('Protected Route');
+        } catch(err){
+            res.send(err)
+        }
     }
 }
